49937359bc45b67792d79de09484a963a148f503
ICT Academy <ict@mainlab14.ictkerala.org>
2023-06-16 16:50:22 +0530

    update

--- src/loginform.jsx
+++ src/loginform.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { useForm } from "react-hook-form";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
 
 export default function Loginform() {
   return (
     <div> 
+        <Container className="mt-5 pt-5 ">
+            <h1 className="text-center">Login Form</h1>
+        </Container>
         <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
       <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Control type="email" placeholder="Enter email"  />
       <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
       </Form.Text>
     </Form.Group>
 
@@ -19,9 +22,9 @@ export default function Loginform() {
       <Form.Label>Password</Form.Label>
       <Form.Control type="password" placeholder="Password" />
     </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
       <Form.Check type="checkbox" label="Check me out" />
-    </Form.Group>
+    </Form.Group> */}
     <Button variant="primary" type="submit">
       Submit
     </Button>
